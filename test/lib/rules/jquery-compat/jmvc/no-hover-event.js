79102b425e5f25d388abf4c5be6d32a25935e5f3
David Godfrey <david.godfrey@frogeducation.com>
2019-09-02 16:13:53 +0100

    feat(jmvc/no-hover-event): add rule

---
+++ test/lib/rules/jquery-compat/jmvc/no-hover-event.js
@@ -0,0 +1,48 @@
+const { RuleTester } = require('eslint')
+
+const rules = require('../../../../../lib/rules')
+
+const ruleTester = new RuleTester()
+
+ruleTester.run(
+  'jquery-compat/jmvc/no-hover-event',
+  rules['jmvc/no-hover-event'],
+  {
+    valid: [
+      {
+        code: '$.Controller("MyController", {}, { "div.foo click": function() {} })'
+      },
+      {
+        code: '$.Controller("MyController", {}, { "div.foo,div.bar click": function() {} })'
+      },
+      {
+        code: '$.Controller("MyController", {}, { "div.foo, div.bar click": function() {} })'
+      },
+      {
+        code: '$.Controller("MyController", {}, { "div.foo, div.bar {click}": function() {} })'
+      }
+    ],
+    invalid: [
+      {
+        code: '$.Controller("MyController", {}, { "div.foo hover": function() {} })',
+        errors: [
+          {
+            messageId: 'no-hover-event',
+            line: 1,
+            column: 45
+          }
+        ]
+      },
+      {
+        code: '$.Controller("MyController", {}, { "div.foo, div.bar hover": function() {} })',
+        errors: [
+          {
+            messageId: 'no-hover-event',
+            line: 1,
+            column: 54
+          }
+        ]
+      }
+    ]
+  }
+)
