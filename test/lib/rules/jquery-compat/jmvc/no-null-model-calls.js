8d94580214b0cf26dc6d0f04020aaadb3cf260fa
David Godfrey <david.godfrey@frogeducation.com>
2019-09-18 17:39:40 +0100

    feat(rules): add jmvc/no-null-model-calls

---
+++ test/lib/rules/jquery-compat/jmvc/no-null-model-calls.js
@@ -0,0 +1,58 @@
+const { RuleTester } = require('eslint')
+
+const rules = require('../../../../../lib/rules')
+
+const ruleTester = new RuleTester()
+
+ruleTester.run(
+  'jquery-compat/jmvc/no-null-model-calls', rules['jmvc/no-null-model-calls'], {
+  valid: [
+    {
+      code: 'Foo.Models.Bar.Baz.findAll({ quux: "corge" })'
+    },
+    {
+      code: 'Foo.Bar.Baz.findAll({ quux: null })'
+    }
+  ],
+  invalid: [
+    {
+      code: `Foo.Models.Bar.Baz.findAll({ baz: null })`,
+      errors: [{
+        messageId: 'no-null-model-calls',
+        line: 1,
+        column: 28,
+        data: {
+          value: null,
+          path: '.baz'
+        }
+      }]
+    },
+    {
+      code: `Foo.Models.Bar.Baz.quux({ corge: undefined })`,
+      errors: [{
+        messageId: 'no-null-model-calls',
+        line: 1,
+        column: 25,
+        data: {
+          value: undefined,
+          path: '.corge'
+        }
+      }]
+    },
+    {
+      code: `var params = {};
+      params.corge = undefined;
+      params.corge = null;
+      Foo.Models.Bar.Baz.quux(params)`,
+      errors: [{
+        messageId: 'no-null-model-calls',
+        line: 4,
+        column: 31,
+        data: {
+          value: null,
+          path: '.corge'
+        }
+      }]
+    }
+  ]
+})
