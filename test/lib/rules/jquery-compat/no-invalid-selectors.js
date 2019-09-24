e1a5d3c716612c9675bc5698381fae5fb5bdcf3e
David Godfrey <david.godfrey@frogeducation.com>
2019-09-24 14:20:17 +0100

    feat(rules): add no-invalid-selectors rule
    attempts to detect invalid selectors being passed to jquery

---
+++ test/lib/rules/jquery-compat/no-invalid-selectors.js
@@ -0,0 +1,101 @@
+const { RuleTester } = require('eslint')
+
+const rules = require('../../../../lib/rules')
+
+const invalidExamples = [
+  `'div[data-value="something"'`,
+  `'#.'`,
+  `'div:nth-child(NaN)'`
+]
+
+const selectorMethods = [
+  "add",
+  "addBack",
+  "children",
+  "closest",
+  "filter",
+  "find",
+  "has",
+  "is",
+  "next",
+  "nextAll",
+  "nextUntil",
+  "not",
+  "parent",
+  "parents",
+  "parentsUntil",
+  "prev",
+  "prevAll",
+  "prevUntil",
+  "siblings"
+]
+
+const ruleTester = new RuleTester()
+
+ruleTester.run('jquery-compat/no-invalid-selectors', rules['no-invalid-selectors'], {
+  valid: selectorMethods
+    .map(methodName => ({
+      code: `$(document).${methodName}("div.className")`
+    })),
+  invalid: /**/selectorMethods
+    .map(methodName =>
+      invalidExamples.map(selector => ({
+        code: `$(document).${methodName}(${selector})`,
+        errors: [{
+          messageId: 'no-invalid-selectors',
+          line: 1,
+          column: `$(document).${methodName}(`.length + 1,
+          data: {
+            selector: selector.slice(1, -1),
+            method: `$.fn.${methodName}()`
+          }
+        }]
+      }))
+    )
+    .reduce((acc, next) => acc.concat(next), [])
+    .concat(
+      invalidExamples
+        .map(selector => ({
+          code: `$(${selector})`,
+          errors: [{
+            messageId: 'no-invalid-selectors',
+            line: 1,
+            column: 3,
+            data: {
+              selector: selector.slice(1, -1),
+              method: '$()'
+            }
+          }]
+        }))
+    )
+    .concat([
+      {
+        code: `$(document).find("div:nth-child(NaN)");
+        `,
+        errors: [{
+          messageId: 'no-invalid-selectors',
+          line: 1,
+          column: 18,
+          data: {
+            selector: "div:nth-child(NaN)",
+            method: '$.fn.find()'
+          }
+        }]
+      },
+      {
+        code: `var bit = "div";
+        var number = NaN;
+        $(document).find(bit + ":nth-child(" + number + ")");
+        `,
+        errors: [{
+          messageId: 'no-invalid-selectors',
+          line: 3,
+          column: 26,
+          data: {
+            selector: "div:nth-child(NaN)",
+            method: '$.fn.find()'
+          }
+        }]
+      }
+    ])
+})
